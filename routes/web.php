72c72a2497822617b5dd74900cae22542e601dbc
Gugun Gunawan <gugun.net21@gmail.com>
2024-11-09 12:48:08 +0700

    -

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/dashboard', [AdminController::class, 'index']);
